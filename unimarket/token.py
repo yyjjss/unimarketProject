ffc33632678014c541865e54907cc1262732a299
yyjjss <ssang632@gmail.com>
2022-03-26 23:07:01 +0900

    파일추가

--- unimarket/token.py
+++
@@ -1,8 +0,0 @@
-import six 
-from django.contrib.auth.tokens import PasswordResetTokenGenerator 
-
-class MemberActivationTokenGenerator(PasswordResetTokenGenerator): 
-    def _make_hash_value(self, dto, timestamp): 
-        return (six.text_type(dto.pk) + six.text_type(timestamp)) + six.text_type(dto.is_active) 
-        
-member_activation_token = MemberActivationTokenGenerator()
